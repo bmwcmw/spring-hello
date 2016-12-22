76ad8c4db76322beaf1f0c06acccc1bc59abb485
CMWZ97I <syncpages@gmail.com>
2016-12-22 01:27:25 +0100

    Add boot application and controller

---
+++ spring-boot/src/main/java/org/boot/Controller.java
@@ -0,0 +1,15 @@
+package org.boot;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
