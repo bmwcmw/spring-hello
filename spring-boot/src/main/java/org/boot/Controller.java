05414ca193dba30f11d7c38f51004c98631b460a
CMWZ97I <syncpages@gmail.com>
2017-01-24 03:01:53 +0100

    Try to add MVC controller 2

--- spring-boot/src/main/java/org/boot/Controller.java
+++ spring-boot/src/main/java/org/boot/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-    @RequestMapping("/")
+    @RequestMapping("/gt1")
     public String index() {
         return "Greetings from Spring Boot!";
     }
