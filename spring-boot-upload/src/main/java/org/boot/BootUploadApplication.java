a498e71433e43f086d9bba786d43d47e7699b620
CMWZ97I <syncpages@gmail.com>
2017-01-24 01:52:05 +0100

    Complete controller and services

--- spring-boot-upload/src/main/java/org/boot/BootUploadApplication.java
+++ spring-boot-upload/src/main/java/org/boot/BootUploadApplication.java
@@ -1,7 +1,16 @@
 package org.boot;
 
+import java.io.File;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 //import org.springframework.context.annotation.PropertySource;
 //import org.springframework.context.annotation.PropertySources;
 
@@ -9,7 +18,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@PropertySources(value = {@PropertySource("classpath:application.properties")})
 public class BootUploadApplication {
 
+    @Value("${multipart.location}/gif/")
+    private String gifLocation;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootUploadApplication.class, args);
 	}
+
+    @PostConstruct
+    private void init() {
+        File gifFolder = new File(gifLocation);
+        if (!gifFolder.exists()) {
+            gifFolder.mkdir();
+        }
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/gif/**")
+                        .addResourceLocations("file:" + gifLocation);
+                super.addResourceHandlers(registry);
+            }
+        };
+    }	
 }
--- spring-boot-upload/src/main/java/org/boot/controller/UploadController.java
+++ spring-boot-upload/src/main/java/org/boot/controller/UploadController.java
@@ -3,7 +3,16 @@ package org.boot.controller;
 import java.io.File;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import javax.inject.Inject;
+
+import org.boot.services.ConverterService;
+import org.boot.services.GifEncoderService;
+import org.boot.services.VideoDecoderService;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FrameGrabber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+
 // curl -F file=@SampleVideo_1280x720_1mb.mp4 -F start=0 -F end=0 -F speed=1 -F repeat=0 localhost:8080/upload
 @RestController
 public class UploadController {
@@ -23,17 +34,36 @@ public class UploadController {
 	
 	@Value("${multipart.location}")
 	private String location;
+
+    @Inject
+    private ConverterService converterService;
+
+    @Inject
+    private GifEncoderService gifEncoderService;
+
+    @Inject
+    private VideoDecoderService videoDecoderService;
 	
 	@RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE)
 	public String upload(@RequestPart("file") MultipartFile file, 
 			@RequestParam("start") int start,
 			@RequestParam("end") int end,
 			@RequestParam("speed") int speed,
-			@RequestParam("repeat") int repeat) throws IllegalStateException, IOException {
+			@RequestParam("repeat") boolean repeat) throws IllegalStateException, IOException, FrameGrabber.Exception {
 		File videoFile = new File(location + File.separator + System.currentTimeMillis() + ".mp4");
 		file.transferTo(videoFile);
 		
 		log.info("Saved file to {}", videoFile.getAbsolutePath());
-		return "";
+		
+		Path output = Paths.get(location + "/gif/" + System.currentTimeMillis() + ".gif");
+
+        FFmpegFrameGrabber frameGrabber = videoDecoderService.read(videoFile);
+        AnimatedGifEncoder gifEncoder = gifEncoderService.getGifEncoder(repeat,
+                (float) frameGrabber.getFrameRate(), output);
+        converterService.toAnimatedGif(frameGrabber, gifEncoder, start, end, speed);
+
+        log.info("Saved generated gif to {}", output.toString());
+
+        return output.getFileName().toString();
 	}
 }
