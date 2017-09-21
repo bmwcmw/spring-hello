8ebc31bcab931087d2bfeddaf285534a9b8c5866
M.C <syncpages@gmail.com>
2017-09-21 16:40:31 +0200

    Add error 500 page

---
+++ spring-boot-spring-security-thymeleaf/src/main/java/com/demo/controller/DefaultController.java
@@ -0,0 +1,92 @@
+package com.demo.controller;
+
+import com.demo.DemoUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String home1() {
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "/registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid DemoUser user) {
+        ModelAndView modelAndView = new ModelAndView();
+
+//        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+//        authorities.add(new SimpleGrantedAuthority("USER"));
+//        UserDetails userDetails = new DemoUser(user.getUsername(), user.getPassword(), authorities);
+//        if (userExists != null) {
+//            bindingResult
+//                    .rejectValue("email", "error.user",
+//                            "There is already a user registered with the email provided");
+//        }
+//        if (bindingResult.hasErrors()) {
+//            modelAndView.setViewName("registration");
+//        } else {
+//            userService.saveUser(user);
+//            modelAndView.addObject("successMessage", "User has been registered successfully");
+//            modelAndView.addObject("user", new DemoUser());
+//            modelAndView.setViewName("registration");
+//
+//        }
+        return modelAndView;
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+
+    @GetMapping("/500")
+    public String error500() {
+        return "/error/500";
+    }
+
+}
--- spring-boot-spring-security-thymeleaf/src/main/java/com/demo/error/MyAccessDeniedHandler.java
+++ spring-boot-spring-security-thymeleaf/src/main/java/com/demo/error/MyAccessDeniedHandler.java
@@ -1,4 +1,4 @@
-package com.mkyong.error;
+package com.demo.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
